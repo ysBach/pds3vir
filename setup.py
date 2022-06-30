aa69fbc63ef5f6dab665bf5a1574d511cf0a5799
ysBach <ysbach93@gmail.com>
2022-06-30 19:32:44 +0900

    add planetarypy into this package

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 
 setup_requires = []
-install_requires = ['numpy', "planetaryimage", "pvl", "six", "astropy"]
+install_requires = ['numpy', "pvl", "six", "astropy"]
 
 classifiers = ["Intended Audience :: Science/Research",
                "Operating System :: OS Independent",
